b43ab68af7a0b8de030bf6a0ea1e320a7648c48c
Yuzhou Mao <yuzhom@umich.edu>
2022-02-25 16:00:54 -0500

    add: impl for migrate method

--- experiment/migrate.py
+++ experiment/migrate.py
@@ -3,8 +3,14 @@
 #
 # Copyright 2021-2022 University of Illinois
 
+import json
+import pickle
+
 from elastic.common.migration_metadata import MigrationMetadata
 from elastic.io.external_storage import ExternalStorage
+
+METADATA_PATH = "./metadata.json"
+OBJECT_PATH_PREFIX = "./obj_"
 
 def migrate(objects_to_migrate, 
             metadata: MigrationMetadata, 
@@ -19,8 +25,14 @@ def migrate(objects_to_migrate,
         objects_to_migrate (List):
             a list of python objects to migrate
         metadata (MigrationMetadata):
-            a JSON file containing migration metadata
+            a JSON structure containing migration metadata
         external_storage (ExternalStorage):
             a wrapper for any storage adapter (local fs, cloud storage, etc.)
     """
-    pass
+    for obj in objects_to_migrate:
+        obj_pickled = pickle.dumps(obj)
+        # FIXME: might be optimizable using batch writes (especially for remote storage using req/resp)
+        storage.write_all(OBJECT_PATH_PREFIX + id(obj), obj_pickled)
+
+    # assume that the list of ids of objects to migrate is already part of the metadata
+    storage.write_all(METADATA_PATH, json.dumps(metadata))
