212e89d246b74dc2a8b814bf22660757e8fa54ff
sdrp713 <sdrp713@gmail.com>
2022-09-29 11:56:32 -0500

    Adding disassembler

---
+++ elastic/test/core/test_disassembler.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021-2022 University of Illinois
+
+import unittest
+from unittest import mock
+import uuid
+from core.graph.graph import DependencyGraph
+from core.graph.node import Node
+from core.graph.node_set import NodeSet, NodeSetType
+from core.notebook.variable_snapshot import VariableSnapshot
+
+VAR_SIZE=1024
+
+
+class TestDisassembler(unittest.TestCase):
+    def test_cell(self):
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
