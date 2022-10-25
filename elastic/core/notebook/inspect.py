104b5e9ec0e79ca0bf7037c90c3eef70f466b23e
Li Billy <libilly@Lis-MacBook-Pro.local>
2022-10-25 11:06:48 -0500

    half working inspect

--- elastic/core/notebook/inspect.py
+++ elastic/core/notebook/inspect.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright 2021-2022 University of Illinois
-
+import networkx as nx
+from matplotlib import pyplot as plt
 
 from elastic.core.graph.graph import DependencyGraph
 
@@ -14,20 +15,36 @@ def inspect(graph: DependencyGraph):
             graph (DependencyGraph): dependency graph representation of the notebook.
     """
 
-    print("---------------------------")
-    print("VSs:")
-    for vs_list in graph.variable_snapshots.values():
-        for vs in vs_list:
-            print(vs.name, vs.version)
+    # Graph to display.
+    display_graph = nx.DiGraph()
 
-    print("---------------------------")
-    print("OEs:")
+    # Dict of nodeset objects to string representation of nodeset contents.
+    nodeset_dict = {}
+
+    # Add string representation of nodesets.
+    for nodeset in graph.input_nodesets + graph.output_nodesets:
+        nodeset_str = ""
+        for vs in nodeset.vs_list:
+            nodeset_str += "(" + vs.name + "," + str(vs.version) + "),"
+        # Trim the trailing comma
+        if len(nodeset_str) > 0:
+            nodeset_str = nodeset_str[:-1]
+        nodeset_dict[nodeset] = nodeset_str
+        display_graph.add_node(nodeset_str)
+
+    # Add string representation of OEs.
     for oe in graph.operation_events:
-        print("---------------------------")
-        print("OE num:", oe.cell_num, " runtime:", oe.cell_runtime)
-        print("sources")
-        for vs in oe.src.vs_list:
-            print("   ", vs.name, vs.version)
-        print("destinations")
-        for vs in oe.dst.vs_list:
-            print("   ", vs.name, vs.version)
+        display_graph.add_edge(nodeset_dict[oe.src], nodeset_dict[oe.dst], cell=str(oe.cell_num))
+
+    # Add inter-cell dependencies.
+    for dst in graph.output_nodesets:
+        for src in graph.input_nodesets:
+            if set(dst.vs_list).intersection(set(src.vs_list)):
+                display_graph.add_edge(nodeset_dict[dst], nodeset_dict[src])
+
+    # Draw the graph.
+    pos = nx.spring_layout(display_graph)
+    nx.draw_networkx(display_graph, pos, with_labels = True)
+    nx.draw_networkx_edge_labels(display_graph, pos)
+
+    plt.show()
