901c9cfcc5dfdcd6446a0b5dce987186f40fb864
ShriyanG <68485981+ShriyanG@users.noreply.github.com>
2023-05-29 01:35:54 -0500

    Complete changes for nbexec module and test case.

---
+++ kishu/tests/test_nbexec.py
@@ -0,0 +1,13 @@
+import os.path
+
+from nbexec import NotebookRunner
+
+
+def test_notebookrunner_basic():
+    cell_indices = [0, 1, 2, 3]
+    path_to_notebook = os.getcwd()
+    notebook_name = "test_basic.ipynb"
+    objects = ["z", "b", "a"]
+    notebook = NotebookRunner(path_to_notebook + "/" + notebook_name)
+    output = notebook.execute(cell_indices, objects)
+    assert output == {"z": 2, "b": 9, "a": 1}
