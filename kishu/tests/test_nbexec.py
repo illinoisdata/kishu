61b51f561fa7508090d1b28934c8feeb6bc9b06d
ShriyanG <68485981+ShriyanG@users.noreply.github.com>
2023-06-07 16:01:22 -0500

    Add test cases.

--- kishu/tests/test_nbexec.py
+++ kishu/tests/test_nbexec.py
@@ -1,5 +1,7 @@
 import os
 
+import numpy as np
+
 from kishu.nbexec import NotebookRunner
 
 
@@ -42,3 +44,33 @@ def test_notebookrunner_empty_cell_list():
         "y": "Hello World!",
         "z": [1, 2, 3, 4, 5],
     }
+
+
+def test_notebookrunner_case_three():
+    cell_indices = [i for i in range(27)]
+    path_to_notebook = os.getcwd()
+    notebook_name = "nbexec_test_case_2.ipynb"
+    objects = ["stable_forest", "stable_loop"]
+    notebook = NotebookRunner(path_to_notebook + "/tests/" + notebook_name)
+    output = notebook.execute(cell_indices, objects)
+    print("output", output)
+    expected = {
+        "stable_forest": np.array(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                [0, 1, 0, 1, 1, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        ),
+        "stable_loop": np.zeros((10, 10)),
+    }
+
+    for key in expected.keys():
+        assert np.array_equal(output[key], expected[key])
