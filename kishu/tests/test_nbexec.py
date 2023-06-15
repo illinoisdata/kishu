bb8b0d7a617257a91ccb359ee887e86a65e06e6e
Shriyan Gosavi <68485981+ShriyanG@users.noreply.github.com>
2023-06-15 02:33:14 -0500

    Add test cases to nbexec.py (#86)
    * Add test cases.
    
    * Add basic commits.
    
    * Complete first test.
    
    * current commit
    
    * removed unnecessary indices
    
    * Added test case and notebook
    
    * Adding new notebook.
    
    * Added test case and notebook.
    
    * Test commit
    
    * Fixed rounding errors.
    
    * Removed swap file =.
    
    ---------
    
    Co-authored-by: ShriyanG <“shriyan.gosavi@gmail.com”>

--- kishu/tests/test_nbexec.py
+++ kishu/tests/test_nbexec.py
@@ -1,5 +1,7 @@
 import os
 
+import numpy as np
+
 from kishu.nbexec import NotebookRunner
 
 
@@ -42,3 +44,58 @@ def test_notebookrunner_empty_cell_list():
         "y": "Hello World!",
         "z": [1, 2, 3, 4, 5],
     }
+
+
+def test_notebookrunner_case_two():
+    cell_indices = [i for i in range(27)]
+    path_to_notebook = os.getcwd()
+    notebook_name = "nbexec_test_case_2.ipynb"
+    objects = ["stable_forest", "stable_loop"]
+    notebook = NotebookRunner(path_to_notebook + "/tests/" + notebook_name)
+    output = notebook.execute(cell_indices, objects)
+    expected = {
+        "stable_forest": np.array(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+                [0, 1, 0, 1, 1, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        ),
+        "stable_loop": np.zeros((10, 10)),
+    }
+
+    for key in expected.keys():
+        assert np.array_equal(output[key], expected[key])
+
+
+def test_notebookrunner_case_three():
+    path_to_notebook = os.getcwd()
+    notebook_name = "nbexec_test_case_3.ipynb"
+    objects = ["mse", "intercept"]
+    notebook = NotebookRunner(path_to_notebook + "/tests/" + notebook_name)
+    output = notebook.execute(None, objects)
+    expected = {"mse": 0.04, "intercept": 0.25}
+
+    assert output == expected
+
+
+def test_notebookrunner_case_four():
+    path_to_notebook = os.getcwd()
+    notebook_name = "nbexec_test_case_4.ipynb"
+    objects = ["mse", "intercept", "estimated_value"]
+    notebook = NotebookRunner(path_to_notebook + "/tests/" + notebook_name)
+    output = notebook.execute(None, objects)
+    expected = {
+        "mse": 0.56,
+        "intercept": -37.02,
+        "estimated_value": -75.31,
+    }
+
+    assert output == expected
