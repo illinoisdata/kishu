56daeb4165fe78c6d06a2f25a75ca5147692c863
fhx <hanxif01@gmail.com>
2023-07-18 16:40:36 +0800

    fix_supawit's_questions

--- kishu/timetravel_fe/src/util/mockdata.ts
+++ kishu/timetravel_fe/src/util/mockdata.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: University of Illinois at Urbana Champaign
  * @Date: 2023-07-16 13:01:19
- * @LastEditTime: 2023-07-18 13:50:18
+ * @LastEditTime: 2023-07-18 16:31:32
  * @FilePath: /src/util/mockdata.ts
  * @Description:
  */
@@ -17,16 +17,16 @@ export const MOCK_HISTORIES = [
     parentOid: -1,
   }),
   new History({
-    oid: 1002,
-    branchId: 1,
-    timestamp: "2023-06-29,15:16:29",
+    oid: 1020,
+    branchId: 2,
+    timestamp: "2023-06-31,15:16:28",
     parentBranchID: 1,
     parentOid: 1000,
   }),
   new History({
-    oid: 1020,
-    branchId: 2,
-    timestamp: "2023-06-31,15:16:28",
+    oid: 1002,
+    branchId: 1,
+    timestamp: "2023-06-29,15:16:29",
     parentBranchID: 1,
     parentOid: 1000,
   }),
@@ -178,6 +178,109 @@ export const MOCK_DETAILED_HISTORIES: Map<Number, History> = new Map([
           oid: 5001,
           version: 1001,
           content: "length_c = 5",
+          execNum: 3,
+        }),
+      ],
+      variables: [
+        new Variable({
+          oid: 6000,
+          variableName: "length_c",
+          version: 1004,
+          state: "5",
+        }),
+        new Variable({
+          oid: 5999,
+          variableName: "length_b",
+          version: 1003,
+          state: "6",
+        }),
+        new Variable({
+          oid: 5998,
+          variableName: "length_a",
+          version: 1003,
+          state: "7",
+        }),
+      ],
+    }),
+  ],
+  [
+    1000,
+    new History({
+      oid: 1000,
+      branchId: 1,
+      timestamp: "2023-06-29,15:16:29",
+      parentBranchID: 1,
+      parentOid: -1,
+      execCell: 5001,
+      codes: [
+        new Cell({
+          oid: 5001,
+          version: 1003,
+          content: "a = 'I like strawberry",
+          execNum: 1,
+        }),
+        new Cell({
+          oid: 5002,
+          version: 1002,
+          content:
+            "print('I enjoy coding, but my aesthetics may be not good enough to design a beautiful page')",
+          execNum: 2,
+        }),
+        new Cell({
+          oid: 5001,
+          version: 1001,
+          content: "oops",
+          execNum: 3,
+        }),
+      ],
+      variables: [
+        new Variable({
+          oid: 6000,
+          variableName: "length_c",
+          version: 1004,
+          state: "5",
+        }),
+        new Variable({
+          oid: 5999,
+          variableName: "length_b",
+          version: 1003,
+          state: "6",
+        }),
+        new Variable({
+          oid: 5998,
+          variableName: "length_a",
+          version: 1003,
+          state: "7",
+        }),
+      ],
+    }),
+  ],
+  [
+    1020,
+    new History({
+      oid: 1020,
+      branchId: 2,
+      timestamp: "2023-06-29,15:16:29",
+      parentBranchID: 1,
+      parentOid: 1000,
+      execCell: 5001,
+      codes: [
+        new Cell({
+          oid: 5001,
+          version: 1003,
+          content: "a = 'another history on the other branch~",
+          execNum: 1,
+        }),
+        new Cell({
+          oid: 5002,
+          version: 1002,
+          content: "print('hahahaha')",
+          execNum: 2,
+        }),
+        new Cell({
+          oid: 5001,
+          version: 1001,
+          content: "oops",
           execNum: 3,
         }),
       ],
