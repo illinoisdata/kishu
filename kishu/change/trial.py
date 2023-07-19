0178c30ca28d9f03e309b846727dffa9767509f8
ribhavs2 <ribhavs2@illinois.edu>
2023-07-19 08:08:42 -0500

    Temporary version of an idgraph implemented in python

---
+++ kishu/change/trial.py
@@ -0,0 +1,116 @@
+import hashlib
+import pandas as pd
+import pickle
+
+def is_pickable(obj):
+    try:
+        if callable(obj):
+            return False
+
+        pickle.dumps(obj)
+        return True
+    except (pickle.PicklingError, AttributeError, TypeError):
+        return False
+    
+
+def get_object_state(obj, visited=None):
+    if visited is None:
+        visited = set()
+
+    if hasattr(obj, "persistent_id"):
+        print(obj, type(obj))
+
+    if id(obj) in visited:
+        return "CYCLIC_REFERENCE"
+
+    if isinstance(obj, (int, float, str, bool, type(None))):
+        return obj
+    
+    elif isinstance(obj, tuple):
+        # visited.add(id(obj))
+        state_representation = []
+        for item in obj:
+            item_state = get_object_state(item, visited)
+            state_representation.append(item_state)
+        return tuple(state_representation)
+
+    elif isinstance(obj, (list, set)):
+        visited.add(id(obj))
+        state_representation = []
+        for item in obj:
+            item_state = get_object_state(item, visited)
+            state_representation.append(item_state)
+        return tuple(state_representation)
+        # return tuple(get_object_state(item, visited | {id(obj)}) for item in obj)
+    
+    elif isinstance(obj, dict):
+        # visited.add(id(obj))
+        dict_inst = obj.copy()
+        for key, value in dict_inst.items():
+            dict_inst[key] = get_object_state(value, visited)
+        
+        return dict_inst
+    
+    elif isinstance(obj, (bytes, bytearray)):
+        return obj
+
+    elif isinstance(obj, type):
+        return str(obj)
+    
+    elif hasattr(obj, '__reduce_ex__'):
+            if is_pickable(obj) and obj.__reduce_ex__(4) == obj.__reduce_ex__(4):
+                visited.add(id(obj))
+                reduced = obj.__reduce_ex__(4)
+
+                if isinstance(reduced, str):
+                    return reduced
+                
+                state_representation = []
+                for item in reduced[1:]:
+                    item_state = get_object_state(item, visited)
+                    state_representation.append(item_state)
+                
+                return tuple(state_representation)
+
+    # If __reduce__
+    elif hasattr(obj, '__reduce__'):
+        if is_pickable(obj) and obj.__reduce__() == obj.__reduce__():
+            visited.add(id(obj))
+            reduced = obj.__reduce__()
+
+            if isinstance(reduced, str):
+                return reduced
+            
+            state_representation = []
+            for item in reduced[1:]:
+                item_state = get_object_state(item, visited)
+                state_representation.append(item_state)
+            
+            return tuple(state_representation)
+
+
+
+
+    elif hasattr(obj, '__dict__'):
+
+        if hasattr(obj, '__getstate__'):
+            if obj.__getstate__() == obj.__getstate__():
+                obj_dict =  obj.__getstate__()
+                for attr_name, attr_value in obj_dict.items():
+                    obj_dict[attr_name] = get_object_state(attr_value, visited)
+                
+                return obj_dict
+            
+        # Use __dict__ only
+        obj_dict = obj.__dict__.copy()
+        for attr_name, attr_value in obj_dict.items():
+            obj_dict[attr_name] = get_object_state(attr_value, visited)
+        
+        return obj_dict
+
+    else:
+        return str(obj)
+
+def get_object_hash(obj):
+    curr_state = get_object_state(obj)
+    return hashlib.md5(str(curr_state).encode('utf-8')).hexdigest()
