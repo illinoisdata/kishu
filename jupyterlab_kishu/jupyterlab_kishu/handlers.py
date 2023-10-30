ede7dee39b48d8a6dfdfb760727dcf7ca8b80434
Supawit Chockchowwat <supawitch22@gmail.com>
2023-10-30 17:19:04 -0500

    Init Kishu as a command palette (#211)

--- jupyterlab_kishu/jupyterlab_kishu/handlers.py
+++ jupyterlab_kishu/jupyterlab_kishu/handlers.py
@@ -1,10 +1,40 @@
 from __future__ import annotations
+import asyncio
+import multiprocessing
 import tornado
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 
 from kishu.commands import KishuCommand, into_json
 from kishu.notebook_id import NotebookId
+
+
+def subp_kishu_init(notebook_path, queue):
+    init_result = KishuCommand.init(notebook_path)
+    queue.put(into_json(init_result))
+
+
+class InitHandler(APIHandler):
+    @tornado.gen.coroutine
+    @tornado.web.authenticated
+    def post(self):
+        input_data = self.get_json_body()
+
+        # We need to run KishuCommand.init in a separate process to unblock Jupyter Server backend
+        # so that our later API calls (e.g., session discovery) are unblocked.
+        init_queue = multiprocessing.Queue()
+        init_process = multiprocessing.Process(
+            target=subp_kishu_init,
+            args=(input_data["notebook_path"], init_queue)
+        )
+        init_process.start()
+        while init_queue.empty():
+            # Awaiting to unblock.
+            yield asyncio.sleep(0.5)
+        init_result_json = init_queue.get()
+        init_process.join()
+
+        self.finish(init_result_json)
 
 
 class LogAllHandler(APIHandler):
@@ -30,6 +60,7 @@ def setup_handlers(web_app):
     base_url = web_app.settings["base_url"]
     kishu_url = url_path_join(base_url, "kishu")
     handlers = [
+        (url_path_join(kishu_url, "init"), InitHandler),
         (url_path_join(kishu_url, "log_all"), LogAllHandler),
         (url_path_join(kishu_url, "checkout"), CheckoutHandler),
     ]
