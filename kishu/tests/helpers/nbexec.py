27268083dbacd2f8360b1af192fcc185d0064787
BillyZhaohengLi <31910858+BillyZhaohengLi@users.noreply.github.com>
2023-11-07 20:42:23 -0600

    Deprecate kishu load string for tests
    Deprecate kishu load string for tests. The original error from switching from kishu load to kishu init was due to a difference in commit ID prefix (load had IDs following 0:x while init's followed 1:x).
    ---------
    
    Co-authored-by: BillyZhaohengLi <zl20@yongjoo-serv-01.cs.illinois.edu>

--- kishu/tests/helpers/nbexec.py
+++ kishu/tests/helpers/nbexec.py
@@ -29,12 +29,10 @@ from kishu.jupyterint import IPYTHON_VARS, KISHU_VARS
 
 NB_DIR: str = os.path.join("tests", "notebooks")
 
-# Breaks test_full_checkout in test_jupyterint when using init_kishu
-KISHU_LOAD_STR: str = "from kishu import load_kishu; load_kishu(); _kishu.set_test_mode()"
 KISHU_INIT_STR: str = "from kishu import init_kishu; init_kishu(); _kishu.set_test_mode()"
 
 
-def get_kishu_checkout_str(cell_num: int, session_num: int = 0) -> str:
+def get_kishu_checkout_str(cell_num: int, session_num: int = 1) -> str:
     return f"_kishu.checkout('{session_num}:{cell_num}')"
 
 
@@ -151,7 +149,7 @@ class NotebookRunner:
         assert cell_num_to_restore >= 2 and cell_num_to_restore <= len(notebook["cells"]) - 1
 
         # create a kishu initialization cell and add it to the start of the notebook.
-        notebook.cells.insert(0, new_code_cell(source=KISHU_LOAD_STR))
+        notebook.cells.insert(0, new_code_cell(source=KISHU_INIT_STR))
 
         # Insert dump session code at middle of notebook after the **cell_num_to_restore**th code cell.
         dumpsession_code_middle = get_dump_namespace_str(self.pickle_file + ".middle")
