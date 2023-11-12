af4b80f9f2ba2f2ebbcc78bbb3f548abcbf1c772
ribhavs2 <ribhavs2@illinois.edu>
2023-11-12 17:19:47 -0600

    Added type annotations to functions

--- kishu/kishu/planning/hash_visitor.py
+++ kishu/kishu/planning/hash_visitor.py
@@ -1,5 +1,7 @@
 import pandas
 import pickle
+import xxhash
+from typing import Tuple, Union
 from kishu.planning.visitor import Visitor
 import kishu.planning.object_state as object_state
 
@@ -16,7 +18,8 @@ def is_pickable(obj) -> bool:
 
 
 class hash_vis(Visitor):
-    def check_visited(self, visited, obj_id, obj_type, include_id, hash_state):
+    def check_visited(self, visited: set, obj_id: int, obj_type: type, include_id: bool,
+                      hash_state: xxhash.xxh32) -> Tuple[bool, Union[xxhash.xxh32, int]]:
         if obj_id in visited:
             hash_state.update(str(obj_type))
             if include_id:
@@ -25,13 +28,13 @@ class hash_vis(Visitor):
         else:
             return False, 0
 
-    def visit_primitive(self, obj, hash_state):
+    def visit_primitive(self, obj, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         hash_state.update(str(type(obj)))
         hash_state.update(str(obj))
         hash_state.update("/EOC")
         return hash_state
 
-    def visit_tuple(self, obj, visited, include_id, hash_state):
+    def visit_tuple(self, obj, visited: set, include_id: bool, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         hash_state.update(str(type(obj)))
         for item in obj:
             object_state.get_object_state(
@@ -40,7 +43,7 @@ class hash_vis(Visitor):
         hash_state.update("/EOC")
         return hash_state
 
-    def visit_list(self, obj, visited, include_id, hash_state):
+    def visit_list(self, obj, visited: set, include_id: bool, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         hash_state.update(str(type(obj)))
         visited.add(id(obj))
         if include_id:
@@ -53,7 +56,7 @@ class hash_vis(Visitor):
         hash_state.update("/EOC")
         return hash_state
 
-    def visit_set(self, obj, visited, include_id, hash_state):
+    def visit_set(self, obj, visited: set, include_id: bool, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         hash_state.update(str(type(obj)))
         visited.add(id(obj))
         if include_id:
@@ -66,7 +69,7 @@ class hash_vis(Visitor):
         hash_state.update("/EOC")
         return hash_state
 
-    def visit_dict(self, obj, visited, include_id, hash_state):
+    def visit_dict(self, obj, visited: set, include_id: bool, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         hash_state.update(str(type(obj)))
         visited.add(id(obj))
         if include_id:
@@ -81,18 +84,18 @@ class hash_vis(Visitor):
         hash_state.update("/EOC")
         return hash_state
 
-    def visit_byte(self, obj, visited, include_id, hash_state):
+    def visit_byte(self, obj, visited: set, include_id: bool, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         hash_state.update(str(type(obj)))
         hash_state.update(obj)
         hash_state.update("/EOC")
         return hash_state
 
-    def visit_type(self, obj, visited, include_id, hash_state):
+    def visit_type(self, obj, visited: set, include_id: bool, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         hash_state.update(str(type(obj)))
         hash_state.update(str(obj))
         return hash_state
 
-    def visit_callable(self, obj, visited, include_id, hash_state):
+    def visit_callable(self, obj, visited: set, include_id: bool, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         hash_state.update(str(type(obj)))
         if include_id:
             visited.add(id(obj))
@@ -101,7 +104,7 @@ class hash_vis(Visitor):
         hash_state.update("/EOC")
         return hash_state
 
-    def visit_custom_obj(self, obj, visited, include_id, hash_state):
+    def visit_custom_obj(self, obj, visited: set, include_id: bool, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         visited.add(id(obj))
         hash_state.update(str(type(obj)))
 
@@ -121,7 +124,7 @@ class hash_vis(Visitor):
             hash_state.update("/EOC")
         return hash_state
 
-    def visit_other(self, obj, visited, include_id, hash_state):
+    def visit_other(self, obj, visited: set, include_id: bool, hash_state: xxhash.xxh32) -> xxhash.xxh32:
         visited.add(id(obj))
         hash_state.update(str(type(obj)))
         if include_id:
