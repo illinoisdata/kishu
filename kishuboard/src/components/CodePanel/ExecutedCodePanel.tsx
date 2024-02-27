1e42809bdef937449079a8d013d03f5d61135e92
hanxi <hanxif01@gmail.com>
2024-02-26 20:29:38 -0600

    Refined UI: 1. fix: press + not unfold 2. fix: Not automatically select the newest commit 3. fix: checkout need to select branch even though there's only one branch 4. change the dot size of the group commit to be the same as normal commits.

--- kishuboard/src/components/CodePanel/ExecutedCodePanel.tsx
+++ kishuboard/src/components/CodePanel/ExecutedCodePanel.tsx
@@ -12,7 +12,7 @@ export function ExecutedCodePanel() {
         <div className="executed-code">
             {props!.selectedCommit!.historyExecCells.map((code, i) => (
                 <div key={i}>
-                    <SingleCell execNumber={(length - i).toString()} content={code.content} cssClassNames={"notebook"}/>
+                    <SingleCell execNumber={(length - i - 1).toString()} content={code.content} cssClassNames={"notebook"}/>
                     <br/>
                 </div>
             ))}
