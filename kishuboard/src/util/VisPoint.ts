91d8e18aa33dfd4920e68a4ff96cf2d04dc51e23
hanxi <hanxif01@gmail.com>
2024-03-20 15:22:57 -0500

    Change the commit graph to display both notebook and var parents. - Change the commit graph to display both notebook and var parents. - Change the menu to only support rollback execution to the ancestor commits of the current commit.

--- kishuboard/src/util/VisPoint.ts
+++ kishuboard/src/util/VisPoint.ts
@@ -12,4 +12,5 @@ export interface VisPoint {
     visPointID: string; // if only one point, it's if it's group_fold, it's the commit id of the first commit in the group
     commit: Commit; // if it's group_fold, it's the first commit in the group
     parentID: string; //visPointID of the parent
+    nbParentID: string; //visPointID of the nbParent
 }
