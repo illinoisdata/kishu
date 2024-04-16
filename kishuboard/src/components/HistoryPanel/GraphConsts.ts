92fb70962844dcfdcb36a04ef6ed00db44af1341
Hanxi Fang <71375959+iq180fq200@users.noreply.github.com>
2024-04-15 21:34:43 -0500

    Font size and margin changes for the git graph. (#354)
    * Make the font size of the commit graph smaller and make the margins larger.
    
    * Assign the commit whose code son is a leaf to be a "branch type" commit. 
    - This fixes bug.

--- kishuboard/src/components/HistoryPanel/GraphConsts.ts
+++ kishuboard/src/components/HistoryPanel/GraphConsts.ts
@@ -1,11 +1,4 @@
-export let FONTSIZE: number
-if (typeof window !== 'undefined') {
-    // If window is defined (i.e., not in a test environment)
-    FONTSIZE = parseFloat(window.getComputedStyle(document.body).fontSize);
-} else {
-    // If window is not defined (i.e., in a test environment)
-    FONTSIZE = 16; // Provide a default value for testing purposes
-}
+export let GRAPHFONTSIZE: number = 13.5;
 export const COMMITHEIGHT: number = 48;
 export const LINESPACING: number = 12;
 export const COMMITRADIUS: number = 5;
@@ -13,4 +6,5 @@ export const MESSAGEMARGINX: number = 20;
 export const MESSAGEMARGINY: number = 0;
 export const CURRENTCOMMITRADUIS: number = 10;
 export const DATEHEADERHEIGHT: number = 30;
+export const MARGINMESSAGE:number = 5;
 export const COLORSPAN = ["#3398fd", "#8d5deb", "#e94698", "#fd8700", "#d75ab6"]
