c11490b62dd11c8efd1448ba9bd7c92eeee75aaf
Supawit Chockchowwat <supawitch22@gmail.com>
2024-10-09 13:05:22 -0500

    Withdraw from IDGraph extension (#371)

--- kishu/lib/__init__.py
+++
@@ -1,4 +0,0 @@
-'''
-This subpackage contains modules for detecting changes to state/objects before and after
-a cell execution.
-'''
--- kishu/setup.py
+++ kishu/setup.py
@@ -12,26 +12,5 @@ class get_numpy_include(object):
         import numpy
         return numpy.get_include()
 
-
-c_idgraph_extension = Extension(
-    "c_idgraph",
-    sources=["lib/idgraphmodule.c", "lib/cJSON.c"],
-    include_dirs=[get_numpy_include()],
-)
-
-visitor_module_extension = Extension(
-    'VisitorModule',
-    sources=[
-        'lib/visitor_c.c',
-        'lib/hash_visitor_c.c',
-        'lib/xxhash.c'
-    ],
-    include_dirs=['/lib/'],
-    extra_compile_args=['-g', '-O0']
-)
-
-
-setup_args = dict(
-    ext_modules=[c_idgraph_extension, visitor_module_extension],
-)
+setup_args = dict()
 setup(**setup_args)
