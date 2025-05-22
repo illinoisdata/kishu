e5155740e98d0148c6376b616c2ae95ad344a68c
Billy Li <31910858+BillyZhaohengLi@users.noreply.github.com>
2025-05-21 23:45:48 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -20,6 +20,7 @@
 project = "Kishu"
 copyright = "2025, CreateLab"
 author = "CreateLab"
+version = "0.3.3"
 
 
 # -- General configuration ---------------------------------------------------
